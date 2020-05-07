5d174bbd8809e8811486549e71223179d82e5ed7
bakir1 <37544754+bakir1@users.noreply.github.com>
2020-05-07 22:29:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
